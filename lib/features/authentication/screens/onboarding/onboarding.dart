3ff0746bb65e230d6a0f430c606b9c2131b6448d
Naveen-Dilsh <naveentype2001@gmail.com>
2025-03-17 21:25:26 +0530

    onboard screen backend part

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/features/authentication/controllers/onboarding_controller.dart';
 import 'package:shopping_app/features/authentication/screens/onboarding/widgets/onboarding_circular.dart';
 import 'package:shopping_app/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart';
 import 'package:shopping_app/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
 import 'package:shopping_app/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
 import 'package:shopping_app/utils/constants/image_strings.dart';
 import 'package:shopping_app/utils/constants/text_strings.dart';
+import 'package:get/get.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(OnboardingController());
+
     return Scaffold(
       body: Stack(
         children: [
           /// Horizontal Scrollable Pages
           PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
             children: const [
               OnBoardingPage(
                 image: TImages.onBoardingImage1,
